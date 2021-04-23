4bdc8ce9f62a63a48e075eadf47a2f11359fe2d5
webe-solutions <76257250+webe-solutions@users.noreply.github.com>
2021-04-23 20:43:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home Test 22/04/2021</h3>"
+    html = "<h3>Sklearn Prediction Home Test 23/04/2021</h3>"
     return html.format(format)
 
 # TO DO:  Log out the prediction value
